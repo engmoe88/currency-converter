d87f69e6c6ca878d6186a815471563b63dceeb47
Mohammed Almukhtar <msalmukhtar@sbgtv.com>
2020-02-14 16:31:49 -0800

    made some changes

---
+++ src/components/ApiInfo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ApiInfo() {
+    return (
+    <span className="api-info">Exchange rate and data in this app is provided by exchangeratesapi.io</span>
+    )
+}
